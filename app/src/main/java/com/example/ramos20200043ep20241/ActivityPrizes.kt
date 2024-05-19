ab59b689c65b19aabf6fc5221f61519da123e950
wramos98 <wilfredo.ramos@tecsup.edu.pe>
2024-05-19 13:11:59 -0500

    Segundo Commit

--- app/src/main/java/com/example/ramos20200043ep20241/ActivityPrizes.kt
+++ app/src/main/java/com/example/ramos20200043ep20241/ActivityPrizes.kt
@@ -3,6 +3,7 @@ package com.example.ramos20200043ep20241
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,12 +19,27 @@ class ActivityPrizes : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-
-
-
+        val etTotalAmount: EditText = findViewById(R.id.etTotalAmount)
+        val etPrimerPlace: EditText = findViewById(R.id.etPrimerPlace)
+        val etSegundoPlace: EditText = findViewById(R.id.etSegundoPlace)
+        val etTercerPlace: EditText = findViewById(R.id.etTercerPlace)
+        val etCuartoPlace: EditText = findViewById(R.id.etCuartoPlace)
+        val btCalcular: Button = findViewById(R.id.btCalcular)
 
         val btGoMenuPriz : Button = findViewById(R.id.btGoMenuPriz)
+
+
+
+
+        btCalcular.setOnClickListener{
+            val primer = 0.25
+            val segundo = 0.15
+            val tercero = 0.10
+            val cuarto =  0.05
+
+
+
+        }
 
         btGoMenuPriz.setOnClickListener{
             val intent = Intent(this,MenuActivity::class.java)
